4f00522e597803d3f8616552c9de2ab6e4b742d1
andrewjmorrell <andrewjmorrell@gmail.com>
2024-02-06 11:43:17 -0700

    cleanup

--- src/main/java/com/arcxp/video/service/AdUtils.kt
+++ src/main/java/com/arcxp/video/service/AdUtils.kt
@@ -2,6 +2,7 @@ package com.arcxp.video.service
 
 import android.net.Uri
 import android.util.Log
+import androidx.media3.common.util.UnstableApi
 import com.arcxp.commons.util.Constants.SDK_TAG
 import com.arcxp.commons.util.DependencyFactory
 import com.arcxp.commons.util.MoshiController.fromJson
@@ -22,6 +23,7 @@ import java.nio.charset.StandardCharsets
 /**
  * @suppress
  */
+@UnstableApi
 internal class AdUtils {
 
     companion object {
