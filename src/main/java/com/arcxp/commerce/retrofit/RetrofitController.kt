7a2fb0012a938f584c8f1bf2dc09a40b29c1d09c
andrewjmorrell <andrewjmorrell@gmail.com>
2024-03-20 14:14:23 -0600

    Fix sales auth

--- src/main/java/com/arcxp/commerce/retrofit/RetrofitController.kt
+++ src/main/java/com/arcxp/commerce/retrofit/RetrofitController.kt
@@ -48,6 +48,9 @@ object RetrofitController {
         .addInterceptor { chain ->
             val requestBuilder = chain.request().newBuilder()
             requestBuilder.addHeader("Content-Type", "application/json")
+            AuthManager.getInstance().accessToken?.let {
+                requestBuilder.addHeader("Authorization", "Bearer $it")
+            }
             chain.proceed(requestBuilder.build())
         }
         .authenticator(AuthAuthenticator())
